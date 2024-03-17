9101c97f71fbaeb5841bdc818ab36f23821b0d53
Wahid Khan <wahidzk0091@gmail.com>
2024-03-17 12:36:05 +0000

    Pissarro: Configure cpusets for dex2oat
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- device.mk
+++ device.mk
@@ -113,6 +113,15 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     libutils-v32
 
+# Dex2oat
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
